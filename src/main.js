6746f5e0c44a4817a11030ab23ec8c7eaca9ebdf
zhangjunling <zhangjunling@autohome.com.cn>
2021-11-04 10:37:01 +0800

    test

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Index from './components';
-//  import ZJL from 'zjl-react-zoom/dist/index';
+// import ZJL from 'zjl-react-zoom';
 // console.log(111,ZJL)
 const App = () => {
   return (
